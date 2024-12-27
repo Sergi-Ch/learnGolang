6df12f70ff1e216af94664135e62c62dfa1b327f
Foxcer <sergeykhairedinov@gmail.com>
2024-12-27 19:36:58 +0300

    first commit on second branch

--- main.go
+++ main.go
@@ -3,19 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	flag := 21
-	if isOd := isOdd(flag); isOd == true {
-		fmt.Println("it's odd")
-
-	} else {
-		fmt.Println("it isn't odd")
-	}
-
-}
-func isOdd(flag int) bool {
-	if flag%2 != 0 {
-		return true
-	} else {
-		return false
-	}
+	i := 0
+	i++
+	fmt.Println(i)
 }
