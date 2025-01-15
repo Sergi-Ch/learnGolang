ce0b5f551dcc93f96f06e7220693d08cb30a8f24
Foxcer <sergeykhairedinov@gmail.com>
2025-01-15 17:12:32 +0300

    structsMedots

--- main.go
+++ main.go
@@ -1,57 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-type Person struct {
-	Name string
-	Age  int
-}
+import "fmt"
 
 func main() {
-	//инициализация структуры с дефолтным значением (первый вариант)
-	var John Person
-	fmt.Printf("default first>> %T , %#v , \n", John, John)
+	definition()
+}
 
-	//инициализация структуры с дефолтным значением (второй вариант)
-	John2 := Person{}
-	fmt.Printf("default second>> %T , %#v , \n", John2, John2)
+type Square struct {
+	Side int
+}
 
-	//изменение полей структуры
-	John.Name = "John"
-	John.Age = 25
-	fmt.Println("заполненные поля у Джона", John)
+func (s Square) Perimeter() {
+	fmt.Printf("%T, %#v \n", s, s)
+	fmt.Printf("Периметр квадрата: %d \n", s.Side*4)
 
-	//инициализация объекта и его полей
+}
 
-	Brad := Person{
-		Name: "Brad",
-		Age:  43,
-	}
-	fmt.Println(Brad)
+func (s *Square) Scale(multiplier int) { // умножение стороны квадрата на множитель
+	fmt.Printf("%T %#v \n", s, s)
+	s.Side *= multiplier
+	fmt.Printf("%T %#v \n", s, s)
+}
 
-	//инициализация сразу в одну строку по порядку полей
-	Volodya := Person{"Volodya", 24}
-	fmt.Println(Volodya)
+func definition() {
+	testStruct := Square{Side: 5}
+	testPStruct := &testStruct
+	secondTestStruct := Square{Side: 2}
 
-	pVolodya := &Volodya         // получение указателя на объект Volodya
-	fmt.Println((*pVolodya).Age) // сначала разыменовываем, чтобы потом обратиться к полю структуры
-	fmt.Println(pVolodya.Age)    //синтаксический сахар go, обычно используют такой метод
+	testStruct.Perimeter()
+	secondTestStruct.Perimeter()
 
-	pIvan := &Person{"Ivan", 10} // создание сразу указателя на объект
-	fmt.Println(pIvan, *pIvan)
-
-	//создание анонимных структур, то есть без имени
-
-	unnamedStruct := struct {
-		Name, LastName, BirdDate string
-	}{
-		Name:     "NoName",
-		LastName: "NoLastName",
-		BirdDate: fmt.Sprintf("%s", time.Now()), //библиотека для работы со временем
-		//или BirdDate: time.Now().String() будет одинаковый результат
-	}
-	fmt.Println(unnamedStruct)
+	testPStruct.Scale(7)
 }
