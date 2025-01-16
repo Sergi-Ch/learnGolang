9816f6ab7d498a7f529cc518d1797c4ec90fa6ca
Foxcer <sergeykhairedinov@gmail.com>
2025-01-17 00:42:37 +0300

    embedded type

--- main.go
+++ main.go
@@ -1,133 +1,90 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-// Определение интерфейса Shape
-type Shape interface {
-	Area() float64
-	Perimeter() float64
+// Определение интерфейса Builder с методом Build
+type Builder interface {
+	Build()
 }
 
-// Структура Circle
-type Circle struct {
-	Radius float64
+// Тип Person, который будет встраиваться в другие типы
+type Person struct {
+	Name string
+	Age  int
 }
 
-// Реализация методов интерфейса Shape для Circle
-func (c Circle) Area() float64 {
-	return math.Pi * c.Radius * c.Radius
+// Тип WoodBuilder, который встраивает тип Person
+type WoodBuilder struct {
+	Person // Встраивание типа Person
 }
 
-func (c Circle) Perimeter() float64 {
-	return 2 * math.Pi * c.Radius
+// Реализация метода Build для типа WoodBuilder
+func (wb WoodBuilder) Build() {
+	fmt.Println("Строю дом из дерева")
 }
 
-// Структура Rectangle
-type Rectangle struct {
-	Width  float64
-	Height float64
+// Тип BrickBuilder, который встраивает тип Person
+type BrickBuilder struct {
+	Person // Встраивание типа Person
 }
 
-// Реализация методов интерфейса Shape для Rectangle
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
+// Реализация метода Build для типа BrickBuilder
+func (bb BrickBuilder) Build() {
+	fmt.Println("Строю дом из кирпича")
 }
 
-func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.Width + r.Height)
+// Тип Building, который встраивает интерфейс Builder
+type Building struct {
+	Builder // Встраивание интерфейса Builder
+	Name    string
 }
 
-// Функция для вывода информации о фигуре
-func PrintShapeInfo(s Shape) {
-	fmt.Printf("Area: %f, Perimeter: %f\n", s.Area(), s.Perimeter())
+/*
+// Пример метода для типа Person (закомментирован)
+func (p Person) printName() {
+    fmt.Println(p.Name)
 }
 
-func interfaceValue() {
-	fmt.Println("func interfaceValue begin")
-	var testInterface Shape
-	fmt.Printf("Type default testIntarface: %T value: %#v \n", testInterface, testInterface)
-	if testInterface == nil {
-		fmt.Printf("testInterface is nil \n")
+// Пример функции для объяснения встраивания (закомментирован)
+func explanation() {
+    builder := WoodBuilder{Person{Name: "Вася", Age: 12}, "Боб"}
+    fmt.Printf("Type: %T, Value: %#v \n", builder, builder)
+    fmt.Println(builder.Person.Age)
+    fmt.Println(builder.Age)
+
+    builder.printName()
+    fmt.Println(builder.Name)
+}
+*/
+
+// Функция useCase демонстрирует использование встраивания и интерфейсов
+func useCase() {
+	// Создание экземпляра Building с WoodBuilder
+	woodenBuilding := Building{
+		Builder: WoodBuilder{Person{
+			Name: "Вася",
+			Age:  40,
+		}},
+		Name: "Деревянная изба",
 	}
-	var unTestInterface *Rectangle
-	testInterface = unTestInterface
-	fmt.Printf("Type testIntarface: %T value: %#v \n", testInterface, testInterface)
-	if testInterface == nil {
-		fmt.Printf("testInterface is nil \n")
-	} else {
-		fmt.Printf("it isn't nil \n")
-	}
-	namedTestIinteface := &Rectangle{Width: 5, Height: 10}
-	fmt.Printf("Type namedTestIntarface: %T value: %#v \n", namedTestIinteface, namedTestIinteface)
-	var perim float64 = namedTestIinteface.Perimeter()
-	fmt.Printf("вызванный метод периметра у namedTestInterdace %f", perim)
+	// Вызов метода Build для WoodBuilder
+	woodenBuilding.Build()
 
+	// Создание экземпляра Building с BrickBuilder
+	brickBuilding := Building{
+		Builder: BrickBuilder{
+			Person{
+				Name: "Петя",
+				Age:  30,
+			},
+		},
+		Name: "Кирпичный дом",
+	}
+	// Вызов метода Build для BrickBuilder
+	brickBuilding.Build()
 }
 
 func main() {
-	// Создаем экземпляры Circle и Rectangle
-	circle := Circle{Radius: 5}
-	bigCircle := Circle{Radius: 10}
-	rectangle := Rectangle{Width: 4, Height: 6}
-
-	// Выводим информацию о фигурах
-	PrintShapeInfo(circle)
-	PrintShapeInfo(bigCircle)
-	PrintShapeInfo(rectangle)
-	interfaceValue()
-	// про утверждение типа
-	var s Shape
-
-	// Присваиваем значение типа Circle переменной s
-	s = circle
-
-	// Используем тип утверждение для доступа к конкретному типу
-	if c, ok := s.(Circle); ok {
-		fmt.Println("Circle Radius:", c.Radius)
-	} else {
-		fmt.Println("s is not a Circle")
-	}
-
-	// Присваиваем значение типа Rectangle переменной s
-	s = rectangle
-
-	// Используем тип утверждение для доступа к конкретному типу
-	if r, ok := s.(Rectangle); ok {
-		fmt.Println("Rectangle Width:", r.Width)
-		fmt.Println("Rectangle Height:", r.Height)
-	} else {
-		fmt.Println("s is not a Rectangle")
-	}
-
-	//using switch
-	s = circle
-
-	// Используем type switch для обработки различных типов
-	switch v := s.(type) {
-	case Circle:
-		fmt.Println("Circle Radius:", v.Radius)
-	case Rectangle:
-		fmt.Println("Rectangle Width:", v.Width)
-		fmt.Println("Rectangle Height:", v.Height)
-	default:
-		fmt.Println("Unknown type")
-	}
-
-	// Присваиваем значение типа Rectangle переменной s
-	s = rectangle
-
-	// Используем type switch для обработки различных типов
-	switch v := s.(type) {
-	case Circle:
-		fmt.Println("Circle Radius:", v.Radius)
-	case Rectangle:
-		fmt.Println("Rectangle Width:", v.Width)
-		fmt.Println("Rectangle Height:", v.Height)
-	default:
-		fmt.Println("Unknown type")
-	}
-
+	// Вызов функции useCase для демонстрации
+	useCase()
 }
