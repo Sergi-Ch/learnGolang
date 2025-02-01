b5ab8d7e8a4191c310fdd61093fc81d1cb915d76
Foxcer <sergeykhairedinov@gmail.com>
2025-02-01 13:51:55 +0300

    concurrency part 1

--- main.go
+++ main.go
@@ -1,39 +1,94 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var defaultMap map[int]string
-	fmt.Printf("value: %#v tupe: %T\n", defaultMap, defaultMap)
+	// Запуск горутины, которая выводит числа от 0 до 99
+	// go showNubmers(100)
 
-	mapWithNew := *new(map[int64]string)
-	fmt.Printf("value: %#v tupe: %T\n", mapWithNew, mapWithNew)
+	// Передача управления другой горутине
+	// runtime.Gosched()
 
-	mapByMake := make(map[string]string)
-	fmt.Printf("value: %#v tupe: %T\n", mapByMake, mapByMake)
+	// Установка максимального количества процессоров, используемых для выполнения горутин
+	// runtime.GOMAXPROCS(1)
 
-	mapByLiteral := map[int]string{1: "first", 2: "second"}
-	fmt.Printf("value: %#v tupe: %T\n", mapByLiteral, mapByLiteral)
+	// Вывод количества доступных процессоров
+	// fmt.Println(runtime.NumCPU())
 
-	fmt.Println(mapByLiteral[1])
+	// Регистрация defer функций, которые будут выполнены перед завершением функции main
+	// defer fmt.Println(1)
+	// defer fmt.Println(2)
+	// fmt.Println("exit")
 
-	value, ok := mapByLiteral[3]
-	fmt.Printf("value: %#v isThereInMap?>> %t\n", value, ok)
+	// Вызов функции sum и вывод результата
+	// println(sum(1, 2))
 
-	mapByLiteral[3] = "Third"
-	fmt.Printf("value: %#v tupe: %T\n", mapByLiteral, mapByLiteral)
+	// Вызов функции deferValues для демонстрации работы defer
+	// deferValues()
 
-	mapByLiteral[3] = "surprise"
-	fmt.Printf("value: %#v tupe: %T\n", mapByLiteral, mapByLiteral)
+	// Вызов функции makePanic для демонстрации работы panic и recover
+	makePanic()
 
-	delete(mapByLiteral, 3)
-	fmt.Printf("value: %#v tupe: %T\n", mapByLiteral, mapByLiteral)
+	// Этот код не будет выполнен, так как makePanic вызывает панику
+	fmt.Println("after panic")
+}
 
-	mapByLiteral[3] = "third"
-	mapByLiteral[4] = "thou"
+// Функция, которая выводит числа от 0 до numb-1
+func showNubmers(numb int) {
+	for i := 0; i < numb; i++ {
+		fmt.Println(i)
+	}
+}
 
-	for key, val := range mapByLiteral {
-		fmt.Printf("key: %d value: %#v \n", key, val)
+// Функция, которая суммирует два числа и умножает результат на 2 с помощью defer
+func sum(x, y int) (sum int) {
+	defer func() {
+		sum *= 2
+	}()
+	sum = x + y
+	return
+}
+
+// Функция, демонстрирующая различные способы использования defer
+func deferValues() {
+	// Правильный вариант: defer регистрирует вывод значения i на каждой итерации
+	for i := 0; i < 10; i++ {
+		defer fmt.Println("first", i)
 	}
 
+	// Неправильный вариант: defer регистрирует вывод значения i, которое изменяется на каждой итерации
+	for i := 0; i < 10; i++ {
+		defer func() {
+			fmt.Println("second", i)
+		}()
+	}
+
+	// Исправленный вариант: defer регистрирует вывод значения k, которое является копией i
+	for i := 0; i < 10; i++ {
+		k := i
+		defer func() {
+			fmt.Println("first fix of second", k)
+		}()
+	}
+
+	// Исправленный вариант: defer регистрирует вывод значения k, которое передается как аргумент
+	for i := 0; i < 10; i++ {
+		defer func(k int) {
+			fmt.Println("second fix of second", k)
+		}(i)
+	}
+}
+
+// Функция, демонстрирующая работу panic и recover
+func makePanic() {
+	defer func() {
+		// Восстановление после паники и вывод значения, переданного в panic
+		panicValue := recover()
+		fmt.Println(panicValue)
+	}()
+
+	// Вызов паники с сообщением "some panic"
+	panic("some panic")
 }
