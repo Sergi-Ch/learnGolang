ed42782620633dfc53f84ebcfe22598b2cac1ba1
Foxcer <sergeykhairedinov@gmail.com>
2025-03-17 15:07:42 +0300

    context and workerPool

--- main.go
+++ main.go
@@ -1,84 +1,99 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"runtime"
+	"sync"
 	"time"
 )
 
 func main() {
-	//baseSelect()
-	//gracefulShutdown()
+	//baseKnowLedge()
+	workerPool()
 }
 
-func baseSelect() {
-	/*	bufferedChan := make(chan string, 3) //2
-			bufferedChan <- "first"
+func baseKnowLedge() {
+	ctx := context.Background()
+	fmt.Println(ctx)
+	ctxD := context.TODO()
+	fmt.Println(ctxD)
 
-			select {
-			case str := <-bufferedChan:
-				fmt.Println("read", str)
-			case bufferedChan <- "second":
-				fmt.Println("write", <-bufferedChan, <-bufferedChan)
-			}
+	WithValue := context.WithValue(ctx, "name", "vasya")
+	fmt.Println(WithValue.Value("name"))
 
-				unbufChan := make(chan int)
+	withCancel, cancel := context.WithCancel(ctx)
+	fmt.Println(withCancel.Err())
+	cancel()
+	fmt.Println(withCancel.Err())
 
-				go func() {
-					time.Sleep(time.Second)
-					unbufChan <- 1
-				}()
+	withDeadLine, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*3))
+	defer cancel()
+	fmt.Println(withDeadLine.Deadline())
+	fmt.Println(withDeadLine.Err())
+	fmt.Println(<-withDeadLine.Done())
 
-					select {
-					//case bufferedChan <- "third":
-					//	fmt.Println("неблокирующая запись")
-					case val := <-unbufChan:
-						fmt.Println("блокирующее чтение", val)
-					case <-time.After(time.Millisecond * 1500):
-						fmt.Println("ожидание вышло")
-						//default:
-						//	fmt.Println("дефолтная ветка")
-					}
+	withTimeOut, cancel := context.WithTimeout(ctx, time.Second*2)
+	defer cancel()
+	fmt.Println(<-withTimeOut.Done())
+}
 
-		resultChan := make(chan int)
-		timer := time.After(time.Second)
+func workerPool() {
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel = context.WithTimeout(ctx, time.Millisecond*3)
 
+	defer cancel()
+
+	wg := &sync.WaitGroup{}
+
+	numbersToProcess, processedNumbers := make(chan int, 5), make(chan int, 5)
+
+	for i := 0; i <= runtime.NumCPU(); i++ {
+		wg.Add(1)
 		go func() {
-			defer close(resultChan)
-
-			for i := 0; i < 1000; i++ {
-				select {
-				case <-timer:
-					fmt.Println("время вышло")
-					return
-				default:
-					time.Sleep(time.Millisecond * 10)
-					resultChan <- i
-				}
-			}
+			defer wg.Done()
+			worker(ctx, numbersToProcess, processedNumbers)
 		}()
-
-		for v := range resultChan {
-			fmt.Println(v)
-		}*/
-}
-
-func gracefulShutdown() {
-	sigChan := make(chan os.Signal, 1)
-
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	timer := time.After(10 * time.Second)
-
-	select {
-	case <-timer:
-		fmt.Println("Время вышло")
-		return
-	case sig := <-sigChan:
-		fmt.Println("Остановилось сигналом: ", sig)
-		return
 	}
 
+	go func() {
+		for i := 0; i < 1000; i++ {
+			numbersToProcess <- i
+		}
+
+		close(numbersToProcess)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(processedNumbers)
+	}()
+
+	var counter int
+	for resultValue := range processedNumbers {
+		counter++
+		fmt.Println(resultValue)
+	}
+
+	fmt.Println(counter)
+
+}
+
+func worker(ctx context.Context, toProcess <-chan int, processed chan<- int) {
+	for {
+		select {
+
+		case <-ctx.Done(): // выход если контекст завершился
+			return
+		case value, ok := <-toProcess:
+			if !ok {
+				return
+			}
+
+			time.Sleep(time.Millisecond) //типо долговременная обработка
+			processed <- value * value
+
+		}
+
+	}
 }
