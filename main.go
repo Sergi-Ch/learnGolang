a5d96523097630ed2f0cd87818ff9c137a5dd655
Foxcer <sergeykhairedinov@gmail.com>
2025-04-08 14:49:04 +0300

    Chan as promise, chan as mutex, wg with error, err group.
    Все примеры демонстрируют разные подходы к работе с горутинами
    
    errgroup предоставляет наиболее элегантный способ управления группой горутин с обработкой ошибок
    
    Каждый пример содержит четкую бизнес-логику:
    
    chanAsPromise - асинхронные операции с ожиданием результатов
    
    chanAsMutex - защита общих данных
    
    WithOutErrorGroup - ручное управление группой горутин
    
    errGroup - автоматическое управление с помощью errgroup
    
    Важно, что в примере с errgroup при ошибке в одной горутине другие автоматически отменяются через контекст
    
    Все горутины корректно обрабатывают отмену через ctx.Done()

--- main.go
+++ main.go
@@ -3,97 +3,166 @@ package main
 import (
 	"context"
 	"fmt"
-	"runtime"
+	"golang.org/x/sync/errgroup"
 	"sync"
 	"time"
 )
 
 func main() {
-	//baseKnowLedge()
-	workerPool()
+	//chanAsPromise()    // Демонстрация использования каналов как Promise
+	//chanAsMutex()      // Демонстрация использования канала как Mutex
+	//WithOutErrorGroup() // Реализация группы горутин без errgroup
+	errGroup() // Реализация группы горутин с errgroup
 }
 
-func baseKnowLedge() {
-	ctx := context.Background()
-	fmt.Println(ctx)
-	ctxD := context.TODO()
-	fmt.Println(ctxD)
+// makeRequest имитирует асинхронный HTTP-запрос
+// num - номер запроса для идентификации
+// Возвращает канал, в который будет отправлен результат
+func makeRequest(num int) <-chan string {
+	responseChan := make(chan string)
 
-	WithValue := context.WithValue(ctx, "name", "vasya")
-	fmt.Println(WithValue.Value("name"))
-
-	withCancel, cancel := context.WithCancel(ctx)
-	fmt.Println(withCancel.Err())
-	cancel()
-	fmt.Println(withCancel.Err())
-
-	withDeadLine, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*3))
-	defer cancel()
-	fmt.Println(withDeadLine.Deadline())
-	fmt.Println(withDeadLine.Err())
-	fmt.Println(<-withDeadLine.Done())
-
-	withTimeOut, cancel := context.WithTimeout(ctx, time.Second*2)
-	defer cancel()
-	fmt.Println(<-withTimeOut.Done())
+	go func() {
+		time.Sleep(time.Second) // Имитация долгого запроса
+		responseChan <- fmt.Sprintf("response numb %d", num)
+	}()
+	return responseChan
 }
 
-func workerPool() {
-	ctx, cancel := context.WithCancel(context.Background())
-	ctx, cancel = context.WithTimeout(ctx, time.Millisecond*3)
+// chanAsPromise демонстрирует паттерн "как Promise" в Go
+// Запускает два асинхронных запроса и ожидает их завершения
+func chanAsPromise() {
+	firstResponceChan := makeRequest(1)  // Первый асинхронный запрос
+	SecondResponceChan := makeRequest(2) // Второй асинхронный запрос
 
-	defer cancel()
+	// Можно выполнять другие операции, пока запросы выполняются
+	fmt.Println("non blocking")
 
-	wg := &sync.WaitGroup{}
+	// Ожидаем завершения обоих запросов (аналог Promise.all)
+	fmt.Println(<-firstResponceChan, <-SecondResponceChan)
+}
 
-	numbersToProcess, processedNumbers := make(chan int, 5), make(chan int, 5)
+// chanAsMutex демонстрирует использование канала как мьютекса
+// для защиты общего ресурса (counter) от гонки данных
+func chanAsMutex() {
+	var counter int
+	// Канал с буфером 1 используется как бинарный семафор
+	mutexChan := make(chan struct{}, 1)
+	wg := sync.WaitGroup{}
 
-	for i := 0; i <= runtime.NumCPU(); i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
+
 		go func() {
 			defer wg.Done()
-			worker(ctx, numbersToProcess, processedNumbers)
+
+			mutexChan <- struct{}{} // Захват "мьютекса"
+			counter++               // Критическая секция
+			<-mutexChan             // Освобождение "мьютекса"
 		}()
 	}
 
-	go func() {
-		for i := 0; i < 1000; i++ {
-			numbersToProcess <- i
-		}
-
-		close(numbersToProcess)
-	}()
-
-	go func() {
-		wg.Wait()
-		close(processedNumbers)
-	}()
-
-	var counter int
-	for resultValue := range processedNumbers {
-		counter++
-		fmt.Println(resultValue)
-	}
-
-	fmt.Println(counter)
-
+	wg.Wait()
+	fmt.Println(counter) // Должно быть 1000
 }
 
-func worker(ctx context.Context, toProcess <-chan int, processed chan<- int) {
-	for {
+// WithOutErrorGroup показывает реализацию группы горутин
+// с обработкой ошибок без использования errgroup
+func WithOutErrorGroup() {
+	var err error
+
+	// Создаем отменяемый контекст для прерывания горутин
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+
+	wg.Add(3) // Ожидаем завершения 3 горутин
+
+	// Горутина 1
+	go func() {
+		time.Sleep(time.Second)
+		defer wg.Done()
+
 		select {
-
-		case <-ctx.Done(): // выход если контекст завершился
+		case <-ctx.Done(): // Проверяем отмену
 			return
-		case value, ok := <-toProcess:
-			if !ok {
-				return
-			}
-
-			time.Sleep(time.Millisecond) //типо долговременная обработка
-			processed <- value * value
-
+		default:
+			fmt.Println("first started")
+			time.Sleep(time.Second)
 		}
+	}()
 
+	// Горутина 2 (возвращает ошибку)
+	go func() {
+		time.Sleep(time.Second)
+		defer wg.Done()
+
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			fmt.Println("second started")
+			err = fmt.Errorf("any error") // Устанавливаем ошибку
+			cancel()                      // Отменяем другие горутины
+		}
+	}()
+
+	// Горутина 3
+	go func() {
+		defer wg.Done()
+
+		select {
+		case <-ctx.Done(): // Будет отменена из-за ошибки в горутине 2
+			return
+		default:
+			fmt.Println("third started")
+			time.Sleep(time.Second)
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println(err) // Выводим ошибку (если была)
+}
+
+// errGroup демонстрирует использование errgroup для управления группой горутин
+// с автоматической отменой при возникновении ошибки
+func errGroup() {
+	// Создаем группу с общим контекстом
+	g, ctx := errgroup.WithContext(context.Background())
+
+	// Горутина 1
+	g.Go(func() error {
+		time.Sleep(time.Second)
+
+		select {
+		case <-ctx.Done(): // Проверяем отмену контекста
+			return nil
+		default:
+			fmt.Println("first started")
+			time.Sleep(time.Second)
+			return nil
+		}
+	})
+
+	// Горутина 2 (возвращает ошибку)
+	g.Go(func() error {
+		fmt.Println("second started")
+		return fmt.Errorf("unexpeted error in request 2")
+	})
+
+	// Горутина 3
+	g.Go(func() error {
+		select {
+		case <-ctx.Done(): // Будет отменена из-за ошибки в горутине 2
+			return nil
+		default:
+			fmt.Println("third started")
+			time.Sleep(time.Second)
+			return nil
+		}
+	})
+
+	// Ожидаем завершения всех горутин
+	// Если была ошибка - выводим ее
+	if err := g.Wait(); err != nil {
+		fmt.Println(err)
 	}
 }
