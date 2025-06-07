a52b80597ac217397ef2304b724e30b3a1049278
Foxcer <sergeykhairedinov@gmail.com>
2025-06-07 19:59:18 +0300

    work with files, bufio, writeTo, ReadTo

--- main.go
+++ main.go
@@ -1,98 +1,141 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
-	// Вызываем функции для демонстрации работы с выводом и вводом
-	outputDemo()
-	inputDemo()
+	// Раскомментируйте нужные функции для тестирования
+	// CreateFile()
+	// WriteTo()
+	// readFrom()
+	// ioCopy()
+	// createFileBufio()
 }
 
-// outputDemo демонстрирует различные способы вывода данных
-func outputDemo() {
-	// 1. Вывод в консоль с помощью fmt.Println
-	// Возвращает количество записанных байт и ошибку
-	count, err := fmt.Println("some text")
-	if err != nil {
-		log.Fatalf("Ошибка при выводе: %v", err)
-	}
-	fmt.Printf("Количество записанных байт: %d\n", count)
+// CreateFile создает файл test.txt и записывает в него числа от 0 до 99
+func CreateFile() {
+	start := time.Now()
 
-	// 2. Форматированный вывод в стандартный поток вывода (stdout)
-	count, err = fmt.Fprintf(os.Stdout, "Stdout from FprintF\n")
-	if err != nil {
-		log.Fatalf("Ошибка при выводе в stdout: %v", err)
-	}
-	fmt.Printf("Количество записанных байт: %d\n", count)
-
-	// 3. Форматированный вывод в стандартный поток ошибок (stderr)
-	count, err = fmt.Fprintf(os.Stderr, "Stderr from FprintF\n")
-	if err != nil {
-		log.Fatalf("Ошибка при выводе в stderr: %v", err)
-	}
-	fmt.Printf("Количество записанных байт: %d\n", count)
-
-	// 4. Создание файла для записи
 	file, err := os.Create("test.txt")
 	if err != nil {
-		log.Fatalf("Ошибка при создании файла: %v", err)
+		log.Fatal(err)
 	}
 	defer func() {
-		// Закрытие файла при завершении функции
-		err := file.Close()
-		if err != nil {
-			log.Fatalf("Ошибка при закрытии файла: %v", err)
+		// Всегда закрываем файл, даже если произошла ошибка при записи
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
 		}
 	}()
 
-	// Запись в файл с помощью fmt.Fprintf
-	message := "hello it's check for writing and reading,\nmy friend"
-	count, err = fmt.Fprintf(file, message)
-	if err != nil {
-		log.Fatalf("Ошибка при записи в файл: %v\n", err)
+	for i := 0; i < 100; i++ {
+		// Используем более идиоматичный цикл for
+		if _, err = file.WriteString(fmt.Sprintf("%d\n", i)); err != nil {
+			log.Fatal(err)
+		}
 	}
-	fmt.Printf("В файл записано %d байт\n", count)
+
+	fmt.Println("CreateFile duration:", time.Since(start))
 }
 
-// inputDemo демонстрирует различные способы ввода данных
-func inputDemo() {
-	var (
-		text1 string
-		text2 string
-	)
-
-	// 1. Чтение из стандартного ввода (консоли)
-	fmt.Println("Введите два слова через пробел:")
-	count, err := fmt.Scan(&text1, &text2)
-	if err != nil {
-		log.Fatalf("Ошибка при чтении из консоли: %v", err)
-	}
-	fmt.Printf("Прочитано: %s %s (количество успешно прочитанных значений: %d)\n",
-		text1, text2, count)
-
-	// 2. Открытие файла для чтения
+// WriteTo читает содержимое файла test.txt и выводит его в stdout
+func WriteTo() {
 	file, err := os.Open("test.txt")
 	if err != nil {
-		log.Panicf("Ошибка при открытии файла: %v", err)
+		log.Fatal(err)
 	}
 	defer func() {
-		// Закрытие файла при завершении функции
-		err := file.Close()
-		if err != nil {
-			log.Fatalf("Ошибка при закрытии файла: %v", err)
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
 		}
 	}()
 
-	// Чтение из файла с помощью fmt.Fscan
-	// Fscan читает до пробелов или переноса строки (с припиской ln)
-	count, err = fmt.Fscan(file, &text1, &text2)
+	count, err := file.WriteTo(os.Stdout)
 	if err != nil {
-		log.Printf("Ошибка при чтении файла: %v", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("Из файла прочитано: %s %s (количество значений: %d)\n",
-		text1, text2, count)
+	fmt.Printf("\nWritten %d bytes\n", count)
+}
+
+// readFrom создает файл test2.txt и копирует в него содержимое test.txt
+func readFrom() {
+	file, err := os.Create("test2.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	origFile, err := os.Open("test.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer func() {
+		if err := origFile.Close(); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	count, err := file.ReadFrom(origFile)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("Copied", count, "bytes")
+}
+
+// ioCopy копирует содержимое из stdin в файл test.txt (но в текущей реализации логика некорректна)
+func ioCopy() {
+	file, err := os.OpenFile("test.txt", os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Здесь логика неверная - мы пытаемся копировать из stdout в файл,
+	// но обычно нужно наоборот. Возможно, вы хотели копировать из файла в stdout?
+	count, err := io.Copy(file, os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Copied", count, "bytes from stdin to file")
+}
+
+// createFileBufio создает файл test3.txt с использованием буферизованной записи
+func createFileBufio() {
+	start := time.Now()
+	file, err := os.Create("test3.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writer := bufio.NewWriter(file)
+	defer func() {
+		// Сначала сбрасываем буфер, потом закрываем файл
+		if err := writer.Flush(); err != nil {
+			log.Panic(err)
+		}
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	for i := 0; i < 100; i++ {
+		if _, err = writer.WriteString(fmt.Sprintf("%d\n", i)); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("createFileBufio duration:", time.Since(start))
 }
